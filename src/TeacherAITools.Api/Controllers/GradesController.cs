d4b778f4c90fb9fe47950250b87d6cacfbac09c4
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-04-07 11:21:31 +0700

    feature/grade-api

---
+++ src/TeacherAITools.Api/Controllers/GradesController.cs
@@ -0,0 +1,62 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TeacherAITools.Application.Common.Exceptions;
+using TeacherAITools.Application.Grades.Common;
+using TeacherAITools.Application.Grades.Queries.GetGrades;
+using TeacherAITools.Application.Grades.Queries.GetModulesByGradeId;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Api.Controllers
+{
+    [Route("api/v{version:apiVersion}/grades")]
+    [ApiVersion(1)]
+    public class GradesController(
+        IMediator mediator,
+        ILogger<GradesController> logger) : ApiController(mediator, logger)
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(Response<List<GetGradeResponse>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                return Ok(await mediator.Send(new GetGradesQuery()));
+            }
+            catch (ApiException e)
+            {
+                return BadRequest(new
+                {
+                    errorCode = e.ErrorCode,
+                    error = e.Error,
+                    errorMessage = e.ErrorMessage
+                });
+            }
+        }
+
+        [HttpGet("{id}/modules")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(Response<GetGradeDetailResponse>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            try
+            {
+                return Ok(await mediator.Send(new GetModulesByGradeIdQuery(id)));
+            }
+            catch (ApiException e)
+            {
+                return NotFound(new
+                {
+                    errorCode = e.ErrorCode,
+                    error = e.Error,
+                    errorMessage = e.ErrorMessage
+                });
+            }
+        }
+    }
+}
---
+++ src/TeacherAITools.Application/Grades/Queries/GetModulesByGradeId/GetModulesByGradeIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TeacherAITools.Application.Grades.Common;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Application.Grades.Queries.GetModulesByGradeId
+{
+    public record GetModulesByGradeIdQuery(int GradeId) : IRequest<Response<GetGradeDetailResponse>>;
+}
