d3441a279e1a51af61a2c5daabe2ebf1c92ef617
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-04-07 10:46:36 +0700

    feature/prompt-api

---
+++ src/TeacherAITools.Infrastructure/Prompts/PromptRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using TeacherAITools.Application.Common.Interfaces.Persistence;
+using TeacherAITools.Domain.Entities;
+using TeacherAITools.Infrastructure.Common.Persistence;
+
+namespace TeacherAITools.Infrastructure.Prompts
+{
+    public class PromptRepository(TeacherAIToolsDbContext dbContext, ILogger logger) : Repository<Prompt>(dbContext, logger), IPromptRepository
+    {
+    }
+}
