d7ac8de03045d0e0fe8cc671fe3d09e028296296
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-04-08 12:38:48 +0700

    feature/note-api

---
+++ src/TeacherAITools.Application/Notes/Queries/GetNotes/GetNotesQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TeacherAITools.Application.Notes.Common;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Application.Notes.Queries.GetNotes
+{
+    public record GetNotesQuery() : IRequest<Response<List<GetNoteResponse>>>;
+}
