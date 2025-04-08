6ea3a2a11a5b547926c336ca7395bcebbcc7ca78
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-04-08 12:38:16 +0700

    feature/week-api

---
+++ src/TeacherAITools.Api/Controllers/WeeksController.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TeacherAITools.Application.Common.Exceptions;
+using TeacherAITools.Application.Weeks.Common;
+using TeacherAITools.Application.Weeks.Queries.GetWeeks;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Api.Controllers
+{
+    [Route("api/v{version:apiVersion}/weeks")]
+    [ApiVersion(1)]
+    public class WeeksController(
+        IMediator mediator,
+        ILogger<WeeksController> logger) : ApiController(mediator, logger)
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(Response<List<GetWeekResponse>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                return Ok(await mediator.Send(new GetWeeksQuery()));
+            }
+            catch (ApiException e)
+            {
+                return BadRequest(new
+                {
+                    errorCode = e.ErrorCode,
+                    error = e.Error,
+                    errorMessage = e.ErrorMessage
+                });
+            }
+        }
+    }
+}
---
+++ src/TeacherAITools.Application/Weeks/Queries/GetWeeks/GetWeeksQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TeacherAITools.Application.Weeks.Common;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Application.Weeks.Queries.GetWeeks
+{
+    public record GetWeeksQuery() : IRequest<Response<List<GetWeekResponse>>>;
+}
