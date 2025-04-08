60733ce831280121a31c0163566193b858460ad7
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-04-08 12:38:33 +0700

    feature/school-year-api

---
+++ src/TeacherAITools.Application/SchoolYears/Queries/GetSchoolYears/GetSchoolYearsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TeacherAITools.Application.SchoolYears.Common;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Application.SchoolYears.Queries.GetSchoolYears
+{
+    public record GetSchoolYearsQuery() : IRequest<Response<List<GetSchoolYearResponse>>>;
+}
