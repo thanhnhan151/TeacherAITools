fea753519814ac8a91e99b3fc625d4ca367486e8
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-05-21 18:01:47 +0700

    feature/curriculum-api

--- src/TeacherAITools.Application/Curriculums/Commands/DeleteCurriculum/DeleteCurriculumCommandHandler.cs
+++ src/TeacherAITools.Application/Curriculums/Commands/DeleteCurriculum/DeleteCurriculumCommandHandler.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using TeacherAITools.Application.Common.Enums;
 using TeacherAITools.Application.Common.Exceptions;
 using TeacherAITools.Application.Common.Extensions;
@@ -7,7 +8,7 @@ using TeacherAITools.Domain.Wrappers;
 
 namespace TeacherAITools.Application.Curriculums.Commands.DeleteCurriculum
 {
-    public class DeleteCurriculumCommandHandler
+    public class DeleteCurriculumCommandHandler : IRequestHandler<DeleteCurriculumCommand, Response<GetCurriculumResponse>>
     {
         private readonly IUnitOfWork _unitOfWork;
 
@@ -16,13 +17,22 @@ namespace TeacherAITools.Application.Curriculums.Commands.DeleteCurriculum
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<Response<GetCurriculumResponse>> Handle(int request, CancellationToken cancellationToken)
+        public async Task<Response<GetCurriculumResponse>> Handle(DeleteCurriculumCommand request, CancellationToken cancellationToken)
         {
-            var curriculumQuery = await _unitOfWork.Curriculums.GetAsync(expression: m => m.CurriculumId == request, disableTracking: true);
+            var curriculumQuery = await _unitOfWork.Curriculums.GetAsync(expression: m => m.CurriculumId == request.Id, disableTracking: true);
 
             var curriculum = curriculumQuery.FirstOrDefault() ?? throw new ApiException(ResponseCode.CURRICULUM_NOT_FOUND);
 
-            await _unitOfWork.Curriculums.UpdateAsync(curriculum);
+            //if (curriculum.IsActive)
+            //{
+            //    curriculum.IsActive = false;
+            //}
+            //else
+            //{
+            //    curriculum.IsActive = true;
+            //}
+
+            await _unitOfWork.Curriculums.DeleteAsync(curriculum);
             await _unitOfWork.CompleteAsync();
 
             return new Response<GetCurriculumResponse>(code: (int)ResponseCode.DELETED_SUCCESS, message: ResponseCode.DELETED_SUCCESS.GetDescription());
