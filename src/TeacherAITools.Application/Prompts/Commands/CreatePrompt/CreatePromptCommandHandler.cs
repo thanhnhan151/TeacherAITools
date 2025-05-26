c5233d2c4c49003821bd9b7c8df5e85c8e462125
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-05-26 15:53:27 +0700

    feature/prompt-api

--- src/TeacherAITools.Application/Prompts/Commands/CreatePrompt/CreatePromptCommandHandler.cs
+++ src/TeacherAITools.Application/Prompts/Commands/CreatePrompt/CreatePromptCommandHandler.cs
@@ -3,24 +3,30 @@ using TeacherAITools.Application.Common.Enums;
 using TeacherAITools.Application.Common.Exceptions;
 using TeacherAITools.Application.Common.Extensions;
 using TeacherAITools.Application.Common.Interfaces.Persistence.Base;
+using TeacherAITools.Application.Common.Interfaces.Services;
 using TeacherAITools.Domain.Entities;
 using TeacherAITools.Domain.Wrappers;
 
 namespace TeacherAITools.Application.Prompts.Commands.CreatePrompt
 {
     public class CreatePromptCommandHandler(
-        IUnitOfWork unitOfWork) : IRequestHandler<CreatePromptCommand, Response<string>>
+        IUnitOfWork unitOfWork,
+        ICurrentUserService currentUserService) : IRequestHandler<CreatePromptCommand, Response<string>>
     {
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly ICurrentUserService _currentUserService = currentUserService;
 
         public async Task<Response<string>> Handle(CreatePromptCommand request, CancellationToken cancellationToken)
         {
+            string userId = _currentUserService.CurrentPrincipal ?? throw new ApiException(ResponseCode.FAILED_AUTHENTICATION);
+
             if (await _unitOfWork.Prompts.IsLessonIdPresentAsync(request.LessonId)) throw new ApiException(ResponseCode.ALREADY_EXISTED_LESSON);
 
             var newPrompt = new Prompt
             {
                 Description = request.Description,
-                LessonId = request.LessonId
+                LessonId = request.LessonId,
+                UserId = Int32.Parse(userId)
             };
 
             var result = await _unitOfWork.Prompts.AddAsync(newPrompt);
