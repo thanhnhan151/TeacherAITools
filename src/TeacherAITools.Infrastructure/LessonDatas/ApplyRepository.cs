d42f1f4ced3dc3c82d518f723d5dbb36d76804e9
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-05-29 23:12:21 +0700

    feature/apply-api

---
+++ src/TeacherAITools.Infrastructure/LessonDatas/ApplyRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using TeacherAITools.Application.Common.Interfaces.Persistence;
+using TeacherAITools.Domain.Entities;
+using TeacherAITools.Infrastructure.Common.Persistence;
+
+namespace TeacherAITools.Infrastructure.LessonDatas
+{
+    public class ApplyRepository(TeacherAIToolsDbContext dbContext, ILogger logger) : Repository<Apply>(dbContext, logger), IApplyRepository
+    {
+    }
+}
