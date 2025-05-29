6c72316a32efc665b9b01b45f15f3035cac2d754
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-05-29 23:10:58 +0700

    feature/knowledge-api

---
+++ src/TeacherAITools.Application/Ultilities/Commands/CreateKnowLedge/CreateKnowLedgeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using TeacherAITools.Application.Common.Enums;
+using TeacherAITools.Application.Common.Extensions;
+using TeacherAITools.Application.Common.Interfaces.Persistence.Base;
+using TeacherAITools.Domain.Entities;
+using TeacherAITools.Domain.Wrappers;
+
+namespace TeacherAITools.Application.Ultilities.Commands.CreateKnowLedge
+{
+    public class CreateKnowLedgeCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateKnowLedgeCommand, Response<string>>
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<Response<string>> Handle(CreateKnowLedgeCommand request, CancellationToken cancellationToken)
+        {
+            var newEntity = new KnowLedge
+            {
+                Goal = request.Goal,
+                TeacherActivities = request.TeacherActivities,
+                StudentActivities = request.StudentActivities,
+                LessonId = request.LessonId,
+                Duration = "12-15",
+                PromptId = null
+            };
+
+            var result = await _unitOfWork.KnowLedeges.AddAsync(newEntity);
+
+            await _unitOfWork.CompleteAsync();
+
+            return new Response<string>(code: (int)ResponseCode.CREATED_SUCCESS, message: ResponseCode.CREATED_SUCCESS.GetDescription());
+        }
+    }
+}
---
+++ src/TeacherAITools.Infrastructure/LessonDatas/KnowLedgeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using TeacherAITools.Application.Common.Interfaces.Persistence;
+using TeacherAITools.Domain.Entities;
+using TeacherAITools.Infrastructure.Common.Persistence;
+
+namespace TeacherAITools.Infrastructure.LessonDatas
+{
+    public class KnowLedgeRepository(TeacherAIToolsDbContext dbContext, ILogger logger) : Repository<KnowLedge>(dbContext, logger), IKnowLedgeRepository
+    {
+    }
+}
