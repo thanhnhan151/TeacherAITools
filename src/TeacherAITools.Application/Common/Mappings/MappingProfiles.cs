b8c15c177b4f09ac6b9f4b13b9b4148e7d6d2747
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-05-30 23:40:26 +0700

    feature/fix-mappings

--- src/TeacherAITools.Application/Common/Mappings/MappingProfiles.cs
+++ src/TeacherAITools.Application/Common/Mappings/MappingProfiles.cs
@@ -154,8 +154,7 @@ namespace TeacherAITools.Application.Common.Mappings
             #region Curriculum
             CreateMap<Curriculum, GetDetailCurriculumResponse>()
                 .ForMember(c => c.Year, c => c.MapFrom(c => c.SchoolYear.Year))
-                .ForMember(c => c.SchoolYearId, c => c.MapFrom(c => c.SchoolYear.SchoolYearId))
-                .ForMember(c => c.Name, c => c.MapFrom(c => $"{c.Name} {c.GradeId}"));
+                .ForMember(c => c.SchoolYearId, c => c.MapFrom(c => c.SchoolYear.SchoolYearId));
             CreateMap<CurriculumDetail, DetailItem>()
                 .ForMember(c => c.CurriculumTopic, c => c.MapFrom(c => c.CurriculumSubSection.CurriculumSection.CurriculumTopic.CurriculumTopicName))
                 .ForMember(c => c.CurriculumSection, c => c.MapFrom(c => c.CurriculumSubSection.CurriculumSection.CurriculumSectionName))
