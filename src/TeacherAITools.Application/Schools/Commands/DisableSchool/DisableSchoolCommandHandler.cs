f75f58212c4e63b2e7203dca0771efa931a02059
thanhnhan151 <truongthanhnhangv@gmail.com>
2025-06-01 06:56:29 +0700

    feature/school-api

--- src/TeacherAITools.Application/Schools/Commands/DisableSchool/DisableSchoolCommandHandler.cs
+++ src/TeacherAITools.Application/Schools/Commands/DisableSchool/DisableSchoolCommandHandler.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using TeacherAITools.Application.Common.Enums;
 using TeacherAITools.Application.Common.Exceptions;
 using TeacherAITools.Application.Common.Extensions;
 using TeacherAITools.Application.Common.Interfaces.Persistence.Base;
 using TeacherAITools.Application.Schools.Common;
+using TeacherAITools.Domain.Common;
 using TeacherAITools.Domain.Wrappers;
 
 namespace TeacherAITools.Application.Schools.Commands.DisableSchool
@@ -16,15 +18,23 @@ namespace TeacherAITools.Application.Schools.Commands.DisableSchool
         {
             var schoolQuery = await _unitOfWork.Schools.GetAsync(expression: u => u.SchoolId == request.Id, disableTracking: true);
 
-            var school = schoolQuery.FirstOrDefault() ?? throw new ApiException(ResponseCode.USER_NOT_FOUND);
+            var school = schoolQuery.Include(s => s.Users.Where(u => u.RoleId != (int)AvailableRole.Administrator)).FirstOrDefault() ?? throw new ApiException(ResponseCode.USER_NOT_FOUND);
 
             if (school.Status)
             {
                 school.Status = false;
+                foreach (var user in school.Users)
+                {
+                    if (user.IsActive) user.IsActive = false;
+                }
             }
             else
             {
                 school.Status = true;
+                foreach (var user in school.Users)
+                {
+                    if (!user.IsActive) user.IsActive = true;
+                }
             }
 
             await _unitOfWork.Schools.UpdateAsync(school);
